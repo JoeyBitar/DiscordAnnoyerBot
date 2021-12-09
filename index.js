fa844d54671409e44e0d92e6d7f975cd134bea97
Joey Bitar <62488559+JoeyBitar@users.noreply.github.com>
2021-12-09 12:47:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client  = new Discord.Client();
 
-const token = "NjkyOTA5NTg0NzU4ODY1OTMw.Xn1ZPQ.2b8m9DyBq6WIsmX9fqTV5iOZpOE";
+const token = "Your token";
 
 client.on("ready",function(){
     console.log("Bot is online!");
@@ -10,18 +10,18 @@ client.on("ready",function(){
 client.on("message", function(msg){
 
 
-    if(msg.author.username == "Heaven"){
+    if(msg.author.username == "Name of the person"){
       if(msg.content == "lol" || msg.content == "LOL" || msg.content == "Lol" || msg.content == "haha"){
-        msg.reply("Was it actually funny Charbel?!?!");
+        msg.reply("Input message here");
       }
-      else if(msg.content == "bruh"){
-        msg.reply("Don't bruh me!");
+      else if(msg.content == "Message"){
+        msg.reply("Message");
       }
-      else if(msg.content == "get on discord"){
-        msg.reply("Nobody want's to join scrub");
+      else if(msg.content == "Message"){
+        msg.reply("Message");
       }
       else{
-        msg.reply("Charbel shut the hell up!",{tts: true});
+        msg.reply("Message",{tts: true});
       }
     }
 })
